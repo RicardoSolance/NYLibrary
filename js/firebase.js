c04336bbf47bec786acc8a6d14c25b2816efdb05
Ricardo <Ricardontutumu@gmail.com>
2022-03-20 11:33:46 +0000

    FIX/firebase config

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyCXGa4sx6GW-naaDKhsxhrKOcyAixFI6kU",
+  authDomain: "nytimesapi-b9bff.firebaseapp.com",
+  projectId: "nytimesapi-b9bff",
+  storageBucket: "nytimesapi-b9bff.appspot.com",
+  messagingSenderId: "1079520698742",
+  appId: "1:1079520698742:web:dff0d7d1c25b68aa765a57"
+};
+ // Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+//const app = initializeApp(firebaseConfig);
+
+console.log("entraa");
